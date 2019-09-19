debbb1d05d24f110f4096cce43e6d6cfe9e420c8
Lauren Dove <elledoveinc@gmail.com>
2019-09-19 05:33:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-#name_hash.each_key do | key , value|
-  #puts key
   key = nil 
   name_hash.each do |k , v|
     if key == nil
